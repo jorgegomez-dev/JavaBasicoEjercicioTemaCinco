80fd51e5731b06767b0adb79c86fa9307a4b4013
jorgegomez-dev <106641101+jorgegomez-dev@users.noreply.github.com>
2022-06-22 19:30:30 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        CocheCRUD cocheCRUD = new CocheCRUDImpl();
+        cocheCRUD.save();
+        cocheCRUD.findAll();
+        cocheCRUD.delete();
+    }
+}
